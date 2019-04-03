2e20a7c0f714c4c696f7d5c12c6a0f2b72e34618
Frank Chen <frankcrestview@hotmail.com>
2019-04-03 00:52:56 -0400

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Frank Chen on 2019-04-03.
+//  Copyright © 2019 Frank Chen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
